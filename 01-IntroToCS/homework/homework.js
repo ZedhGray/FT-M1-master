079b497a8d738b1b4d9d27ae91e3db82c49a85c5
Eduardo Hernandez <zohanshane@gmail.com>
2023-10-23 09:39:37 -0600

    homeworkOne

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,14 +1,41 @@
-'use strict';
+'use strict'
 
 function BinarioADecimal(num) {
-   return num.toString(2)
+  //Usando metodos
+  // return parseInt(num, 2)
+  //Usando un ciclo
+  let str = num.split('')
+  let result = 0
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i] * Math.pow(2, str.length - 1 - i)
+  }
+  return result
 }
+// 3 a binario es "111"
+/*
+7 / 2 resto 1  7 / 2   2x3 el resto es 1
+3 / 2 resto 1  3 / 2     2x1 el resto es 1
+1 / 2 resto 1  1 / 2     2x0 el resto es 1
 
+12 / 2  2x6 el resto es 0
+6 / 2 2x3 el resto es 0
+3 / 2 2x1 el resto es 1
+1 / 2 2x0 el resto es 1
+
+*/
 function DecimalABinario(num) {
-   return parseInt(num, 2)
+  //Usando metodos
+  // return num.toString(2)
+  //Usando ciclos
+  let binario = ''
+  while (num !== 0) {
+    binario = (num % 2) + binario
+    num = Math.floor(num / 2)
+  }
+  return binario
 }
 
 module.exports = {
-   BinarioADecimal,
-   DecimalABinario,
-};
+  BinarioADecimal,
+  DecimalABinario,
+}
--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -13,7 +13,9 @@ nuevoContador()     // 2
 const otroContador = counter()
 otroContador()      // 1
 otroContador()      // 2 */
-function counter() {}
+function counter() {
+  
+}
 
 /* Ejercicio 2
 Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback 
